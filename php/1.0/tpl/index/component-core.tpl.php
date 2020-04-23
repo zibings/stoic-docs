d5cfaba6350a91ee2cf1f82762ace7d64d7d7656
Andrew Male <mamale@outlook.com>
2020-04-23 04:18:09 -0400

    Working on core component (chains)

--- php/1.0/tpl/index/component-core.tpl.php
+++ php/1.0/tpl/index/component-core.tpl.php
@@ -3,13 +3,13 @@
 	'pageSection' => $pageSection ?? null,
 	'breadcrumbs' => [
 		['text' => 'Home', 'queryVars' => ['page' => 'toc'], 'active' => false],
-		['text' => 'Quick Start', 'queryVars' => ['page' => 'quick-start'], 'active' => true]
+		['text' => 'Core Component', 'queryVars' => ['page' => 'component-core'], 'active' => true]
 	],
-	'title' => 'Stoic:PHP - Quick Start'
+	'title' => 'Stoic:PHP - Core Component'
 ]); ?>
 
 					<div id="doc-header" class="doc-header text-center">
-						<h1 class="doc-title"><i class="icon fa fa-paper-plane"></i> Quick Start</h1>
+						<h1 class="doc-title"><i class="icon fa fa-paper-plane"></i> Core Component</h1>
 						<div class="meta btn-group">
 							<div class="pt-1 mr-4 my-auto" style="height: 32px">
 								<i class="far fa-clock"></i> Last updated: <?=date('F jS, Y', filemtime(__FILE__))?>
@@ -32,164 +32,52 @@
 					<div class="doc-body row">
 						<div class="doc-content col-md-9 col-12 order-1">
 							<div class="content-inner">
-								<section id="installation-section" class="doc-section">
-									<h2 class="section-title">Installation</h2>
-
+								<section id="core-brief" class="doc-section">
 									<div class="section-block">
 										<p>
-											Installation can be done using <a href="//getcomposer.org" target="_blank">Composer</a>/<a href="//packagist.org" target="_blank">Packagist</a>.  Simply enter the following commands in your shell
-											and all of the Stoic components will be installed for a website to be built:
-										</p>
-
-										<div class="code-block">
-											<h6>Require 'Web' component with Composer:</h6>
-
-											<p>
-												<code>composer require stoic/web</code>
-											</p>
-										</div>
-
-										<div class="code-block">
-											<h6>Initialize the default Stoic directories:</h6>
-
-											<p>
-												<code>vendor/bin/stoic-create</code>
-											</p>
-										</div>
-									</div><!--//section-block-->
-								</section><!--//doc-section-->
-
-								<section id="folder-structure" class="doc-section">
-									<h2 class="section-title">Folder Structure</h2>
-
-									<div class="section-block">
-										<p>
-											Stoic will have created the following folder structure for your project:
-										</p>
-
-										<div class="code-block">
-											<code>
-												~/inc/classes/<br />
-												~/inc/repositories/<br />
-												~/inc/utilities/<br />
-												~/index.php<br />
-												~/siteSettings.json
-											</code>
-										</div>
-									</div>
-
-									<div id="classes-folder" class="section-block">
-										<h6>Classes Folder</h6>
-
-										<p>
-											This folder should contain any class definitions.  Any files with the extension <code>.cls.php</code> in this folder will be automatically included when Stoic is loaded.
-										</p>
-
-										<p>
-											For more information on classes, click <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'concepts-classes'])?>">here</a>.
-										</p>
-									</div>
-
-									<div id="repositories-folder" class="section-block">
-										<h6>Repositories Folder</h6>
-
-										<p>
-											This folder should contain any repository classes, which normally provide methods for returning data in the form of classes.
-											Any files with the extension <code>.rpo.php</code> in this folder will be automatically included when Stoic is loaded.
-										</p>
-
-										<p>
-											For more information on repositories, click <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'concepts-repositories'])?>">here</a>.
-										</p>
-									</div>
-
-									<div id="utilities-folder" class="section-block">
-										<h6>Utilities Folder</h6>
-
-										<p>
-											This folder should contain any files that provide utility functionality for your site.
-											Any files with the extension <code>.utl.php</code> in this folder will be automatically included when Stoic is loaded.
-										</p>
-
-										<p>
-											For more information on utility files, click <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'concepts-utilities'])?>">here</a>.
-										</p>
-									</div>
-
-									<div id="index-file" class="section-block">
-										<h6>Index File</h6>
-
-										<p>
-											This file is a quick example of how Stoic can be loaded and used as an entry-point for a website.
-										</p>
-
-										<p>
-											For more information on entry-points and Stoic, click <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'concepts-entry-points'])?>">here</a>.
-										</p>
-									</div>
-
-									<div id="settings-file" class="section-block">
-										<h6>Settings File</h6>
-
-										<p>
-											The settings file <code>siteSettings.json</code> contains the default configuration values for the site.
-											The settings file is setup using the <a href="//github.com/AndyM84/config-migration" target="_blank">Config Migration</a> library.
+											The <em>Core</em> component contains the most basic pieces of Stoic:PHP.  Every other component builds on
+											top of these pieces in some shape or form.  You of course aren't required to use them, but they are there
+											for your convenience as much as they are for Stoic's.
 										</p>
 									</div>
 								</section>
 
-								<section id="example-app" class="doc-section">
-									<h2 class="section-title">Quick Example Webpage</h2>
+								<section id="core-components" class="doc-section">
+									<h2 class="section-title">Included Features</h2>
 
 									<div class="section-block">
 										<p>
-											Replace the contents of the <i>index.php</i> file:
+											The following features are included within the <em>Core</em> component:
 										</p>
 
-										<pre><code class="language-php">&lt;?php
-
-  // Pull in all files via Composer autoload
-  require(&quot;vendor/autoload.php&quot;);
-
-  // Grab the Stoic class for use, setting the 'root' directory to the current directory
-  use Stoic\Web\Stoic;
-  $stoic = Stoic::getInstance(&quot;./&quot;);
-
-  // Retrieve the ParameterHelper for $_GET from the request
-  $get = $stoic->getRequest()->getGet();
-
-  // Retrieve the ParameterHelper for $_SERVER from the request
-  $server = $stoic->getRequest()->getServer();
-
-  // Output some random information
-  echo(&quot;Hello, World!&lt;br /&gt;&quot;);
-  echo(&quot;I am {$server->get(&apos;REMOTE_ADDR&apos;)}&quot;);
-
-  // If we have a magical $_GET variable, show it
-  if ($get-&gt;has(&apos;magic_var&apos;)) {
-    echo(&quot;&lt;br /&gt;Secret Message: {$get->get(&apos;magic_var&apos;)}&quot;);
-  }
-
-</code></pre>
-
-										<p>
-											Which will produce something similar to:
-										</p>
-
-										<code>
-											Hello, World!<br />
-											I am ::1
-										</code>
-
-										<p class="mt-2">
-											If you visit the page and add <code>?magic_var=test</code>, the following will be produced:
-										</p>
-
-										<code>
-											Hello, World!<br />
-											I am ::1<br />
-											Secret Message: test
-										</code>
+										<div class="table-responsive">
+											<table class="table">
+												<thead>
+													<tr>
+														<th>Feature</th>
+														<th>Description</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+														<td><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-chains'])?>">Chains</a></td>
+														<td>Execution system for pub/sub, event, or chain of responsibility patterns</td>
+													</tr>
+													<tr>
+														<td><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-logging'])?>">Logging</a></td>
+														<td>PSR-3 compliant logging system better suited for configured output options</td>
+													</tr>
+													<tr>
+														<td><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-returnhelper'])?>">ReturnHelper</a></td>
+														<td>Utility that provides more detailed success/fail return values</td>
+													</tr>
+													<tr>
+														<td><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-enumbase'])?>">EnumBase</a></td>
+														<td>Utility that provides some general functionality for creating enumerated class values</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
 									</div>
 								</section>
 
@@ -198,8 +86,8 @@
 
 									<div class="section-block">
 										<p>
-											Continue to read about <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'concepts'])?>">concepts</a> used within Stoic, or visit the
-											<a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'toc'])?>">Table of Contents</a>.
+											Continue to read about <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-chains'])?>">chains</a>
+											within the <em>Core</em> component, or visit the <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'toc'])?>">Table of Contents</a>.
 										</p>
 									</div>
 								</section>
@@ -209,18 +97,7 @@
 						<div class="doc-sidebar col-md-3 col-12 order-0 d-none d-md-flex">
 							<div id="doc-nav" class="doc-nav">
 								<nav id="doc-menu" class="nav doc-menu flex-column sticky">
-									<a class="nav-link scrollto" href="#installation-section">Installation</a>
-									<a class="nav-link scrollto" href="#folder-structure">Folder Structure</a>
-
-									<nav class="doc-sub-menu nav flex-column">
-										<a class="nav-link scrollto" href="#classes-folder">Classes</a>
-										<a class="nav-link scrollto" href="#repositories-folder">Repositories</a>
-										<a class="nav-link scrollto" href="#utilities-folder">Utilities</a>
-										<a class="nav-link scrollto" href="#index-file">Index</a>
-										<a class="nav-link scrollto" href="#settings-file">Settings</a>
-									</nav>
-
-									<a class="nav-link scrollto" href="#example-app">Example</a>
+									<a class="nav-link scrollto" href="#core-components">Components</a>
 									<a class="nav-link scrollto" href="#whats-next">What's Next</a>
 								</nav><!--//doc-menu-->
 							</div>
---
+++ php/1.0/tpl/index/core-chains-dispatches.tpl.php
@@ -0,0 +1,175 @@
+<?php $this->layout('shared::master', [
+	'page' => $page,
+	'pageSection' => $pageSection ?? null,
+	'breadcrumbs' => [
+		['text' => 'Home', 'queryVars' => ['page' => 'toc'], 'active' => false],
+		['text' => 'Core Component', 'queryVars' => ['page' => 'component-core'], 'active' => false],
+		['text' => 'Chains', 'queryVars' => ['page' => 'core-chains'], 'active' => false],
+		['text' => 'Dispatches', 'queryVars' => ['page' => 'core-chains-dispatches'], 'active' => true]
+	],
+	'title' => 'Stoic:PHP - Dispatches'
+]); ?>
+
+					<div id="doc-header" class="doc-header text-center">
+						<h1 class="doc-title"><i class="icon fa fa-paper-plane"></i> Dispatches</h1>
+						<div class="meta btn-group">
+							<div class="pt-1 mr-4 my-auto" style="height: 32px">
+								<i class="far fa-clock"></i> Last updated: <?=date('F jS, Y', filemtime(__FILE__))?>
+							</div>
+
+							<div class="pt-1 mr-1 my-auto" style="height: 32px">
+								Release:
+							</div>
+
+							<div class="h-100">
+								<div class="dropdown">
+									<button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button" id="versionDropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										v1.0
+									</button>
+								</div>
+							</div>
+						</div>
+					</div><!--//doc-header-->
+					
+					<div class="doc-body row">
+						<div class="doc-content col-md-9 col-12 order-1">
+							<div class="content-inner">
+								<section id="core-brief" class="doc-section">
+									<div class="section-block">
+										<p>
+											An execution system inspired by the chain-of-responsibility design pattern.
+										</p>
+									</div>
+								</section>
+
+								<section id="chain-concept" class="doc-section">
+									<h2 class="section-title">Concept</h2>
+
+									<div class="section-block">
+										<p>
+											Similar to the chain-of-responsibility pattern, chains are groups of one (1) or more processing objects -
+											called <em>nodes</em> - which receive data as it passes along the group, or chain.
+										</p>
+
+										<p>
+											The entire system revolves around the <code>ChainHelper</code> class.  It facilitates the linking of
+											processing nodes together and sending data along the 'chain' of nodes to be processed in order.  Nodes must
+											all derive from the <code>NodeBase</code> abstract class, and the data sent traversing a chain must derive
+											from the <code>DispatchBase</code> abstract class.
+										</p>
+									</div>
+								</section>
+
+								<section id="chain-example" class="doc-section">
+									<h2 class="section-title">Quick Example</h2>
+
+									<div class="section-block">
+										<p>
+											Before going any further, let's see a (very simplistic) chain in action:
+										</p>
+
+										<div class="code-block">
+											<pre class="language-php"><code>&lt;?php
+
+	use Stoic\Chain\DispatchBase;
+	use Stoic\Chain\NodeBase;
+	use Stoic\Chain\ChainHelper;
+
+	class IncrementDispatch extends DispatchBase {
+		public function initialize($input) {
+			$this->makeValid();
+
+			return;
+		}
+
+		public function increment($number) {
+			return ++$number;
+		}
+	}
+
+	class IncrementNode extends NodeBase {
+		public function __construct() {
+			$this->setKey('incrementNode');
+			$this->setVersion('1.0.0');
+
+			return;
+		}
+
+		public function process($sender, DispatchBase &$dispatch) {
+			if (!($dispatch instanceof IncrementDispatch)) {
+				return;
+			}
+
+			$dispatch->setResult($dispatch->increment(1));
+
+			return;
+		}
+	}
+
+	$chain = new ChainHelper();
+	$chain->linkNode(new IncrementNode());
+
+	$dispatch = new IncrementDispatch();
+	$dispatch->initialize(null);
+
+	$success = $chain->traverse($dispatch);
+	$results = $dispatch->getResults();
+
+	print_r($results);
+</code>
+</pre>
+										</div>
+
+										<p>
+											The above example creates two classes:
+										</p>
+
+										<ol>
+											<li><code>IncrementDispatch</code> - A dispatch class that can increment a number</li>
+											<li><code>IncrementNode</code> - A node class that asks the dispatch to increment and store a number</li>
+										</ol>
+
+										<p>
+											This example isn't very useful, but it does exercise all parts of the Chain system.  For more details on
+											the system, read on!
+										</p>
+									</div>
+								</section>
+
+								<section id="chain-reading" class="doc-section">
+									<h2 class="section-title">Further Reading</h2>
+
+									<div class="section-block">
+										<ul>
+											<li><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-chains-dispatches'])?>">Dispatches</a></li>
+											<li><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-chains-nodes'])?>">Nodes</a></li>
+											<li><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-chains-chainhelper'])?>">ChainHelper</a></li>
+											<li><a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-chains-examples'])?>">Examples</a></li>
+										</ul>
+									</div>
+								</section>
+
+								<section id="whats-next" class="doc-section">
+									<h2 class="section-title">Next Up</h2>
+
+									<div class="section-block">
+										<p>
+											Continue to read about <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'core-chains-dispatches'])?>">dispatches</a>,
+											or visit the <a href="<?=$page->getAssetPath('~/php/1.0/', ['page' => 'toc'])?>">Table of Contents</a>.
+										</p>
+									</div>
+								</section>
+							</div><!--//content-inner-->
+						</div><!--//doc-content-->
+
+						<div class="doc-sidebar col-md-3 col-12 order-0 d-none d-md-flex">
+							<div id="doc-nav" class="doc-nav">
+								<nav id="doc-menu" class="nav doc-menu flex-column sticky">
+									<a class="nav-link scrollto" href="#chain-concept">Concept</a>
+									<a class="nav-link scrollto" href="#chain-example">Example</a>
+									<a class="nav-link scrollto" href="#chain-reading">Further Reading</a>
+									<a class="nav-link scrollto" href="#whats-next">What's Next</a>
+								</nav><!--//doc-menu-->
+							</div>
+						</div><!--//doc-sidebar-->
+					</div><!--//doc-body-->
